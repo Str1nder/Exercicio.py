126a53bbc4e56582257860a65f978b7c9a9645f0
Str1nder <rm552238@fiap.com.br>
2023-03-17 21:35:32 -0300

    Envio

--- Exercicio.py
+++ Exercicio.py
@@ -2,11 +2,9 @@ user = ["Guilherme", "Gustavo", "Rodrigo"]
 senha = ["g111", "g222", "r333"]
 
 input_user = input("Entre com o nome de usuário: ").lower()
-input_nome = input("Entre com a senha do usuário: ").lower()
+input_senha = input("Entre com a senha do usuário: ").lower()
 
 for index in range(0,len(user)):
-    if (input_user == use[index]) and (input_senha == senha[index]):
+    if (input_user == user[index]) and (input_senha == senha[index]):
         print("Sucesso")
-        quit()
-
-print("Dados incorretos.")
+    else: ("Dados incorretos.")
